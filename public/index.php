b1b6ee76fa9df5e2f7c1e67104432b3a1c4b86fb
Rangel Uzunov <rangelcho1900@gmail.com>
2026-09-21 13:21:03 +0300

    Add PHP CS Fixer and PHPStan to development dependencies; update configuration and scripts

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return function (array $context) {
+    $environment = is_string($context['APP_ENV'] ?? null) ? $context['APP_ENV'] : 'prod';
+    $debug = isset($context['APP_DEBUG']) ? (bool) $context['APP_DEBUG'] : false;
+
+    return new Kernel($environment, $debug);
 };
